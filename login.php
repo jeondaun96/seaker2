8a9d3acd4f18496fe29fde20137810979779adf0
이하정 <961210eee@naver.com>
2019-08-06 18:33:29 +0900

    Initial commit

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SEAKER</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/jquery.backstretch.min.js"></script>
+
+    <style>
+
+
+      h1 {
+        font-size:2.5em;
+        font-family:"Oswald", sans-serif;
+        text-shadow:1px 1px 2px black;
+      }
+
+    </style>
+  </head>
+  <body>
+
+    <!--로그인폼 시작-->
+  
+      <h1>LOGIN</h1>
+      <form action="#" class="form-group">
+
+            <div class="input-group">
+                <span class="input-group-addon"> <span class="glyphicon glyphicon-user"> </span> </span>
+                <input type="text" class="form-control input-sm" placeholder="아이디">
+            </div>
+            <br>
+            <div class="input-group">
+                <span class="input-group-addon">  <span class="glyphicon glyphicon-lock"></span> </span>
+                <input type="text" class="form-control input-sm" placeholder="비밀번호">
+            </div>
+            <br>
+                <input type="submit" class="btn btn-default btn-block" value="로그인">
+
+
+
+      <div class="text-center">
+        <a href="http://localhost/project/signin.php">회원가입</a>
+          ㅣ
+        <a href="http://localhost/project/explore.php">아이디와 비밀번호 찾기</a>
+      </div>
+      </form>
+      <!--로그인폼 끝-->
+
+
+  </body>
+</html>
